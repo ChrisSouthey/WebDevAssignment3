645e6361093e9312e7dba6346497c08653002e83
ChrisSouthey <chsouthey@email.neit.edu>
2026-03-05 00:05:08 -0500

    Metro boomin make it boom

--- public/js/filefetch.js
+++
@@ -1,9 +0,0 @@
-fetch("/api/todo")
-    .then(res => res.json())
-    .then(
-        data => {
-        document.getElementById("todoList").textContent = JSON.stringify(data, null, 2);
-    })
-    .catch(err => {
-        console.error("Error fetching data: ", err);
-    });
